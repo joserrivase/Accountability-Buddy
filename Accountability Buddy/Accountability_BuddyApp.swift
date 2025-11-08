a437513f779603c1eb11de30c124f2348a41ffe1
Jose Rivas <jose.r.rivas.e@gmail.com>
2025-11-08 14:18:42 -0500

    Initial Commit

---
+++ Accountability Buddy/Accountability_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Accountability_BuddyApp.swift
+//  Accountability Buddy
+//
+//  Created by Jose Rivas on 11/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Accountability_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Accountability BuddyTests/Accountability_BuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  Accountability_BuddyTests.swift
+//  Accountability BuddyTests
+//
+//  Created by Jose Rivas on 11/8/25.
+//
+
+import Testing
+@testable import Accountability_Buddy
+
+struct Accountability_BuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
